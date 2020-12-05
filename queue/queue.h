1d13c72e1af367013911f470fc778874761b726e
Sindeguru <mrSinguru@gmail.com>
2020-12-06 02:03:04 +0300

    Final version of queue

--- queue/queue.h
+++ queue/queue.h
@@ -7,7 +7,7 @@ class TQueue
 {
 protected:
 	int size;
-	T** arr;
+	T* arr;
 	int start;
 	int finish;
 
@@ -17,45 +17,76 @@ public:
 	TQueue(TQueue& q);
 	~TQueue();
 
-	void push(T& s);
-	T pop();
-
-	//int full(); //empty
+	void push(T s);
+	T pop(); //Вывод последнего элемента и его удаление
+	bool empty();
 
 };
 
 template<class T>
 inline TQueue<T>::TQueue()
 {
-
+	arr = new T * [1];
+	start = 0;
+	finish = 0;
+	size = 1;
+	arr[0] = 0;
 }
 
 template<class T>
 inline TQueue<T>::TQueue(int size)
 {
-
+	if (size < 0) throw logic_error("size < 0");
+	arr = new T [size];
+	this->size = size;
+	start = 0;
+	finish = 0;
+	for (int i = 0; i < size; i++)
+		arr[i] = 0;
 }
 
 template<class T>
 inline TQueue<T>::TQueue(TQueue& q)
 {
-
+	this->size = q.size;
+	this->arr = new T * [q.size];
+	for (int i = 0; i < q.size; i++)
+		this->arr[i] = q.arr[i];
+	this->start = q.start;
+	this->finish = q.finish;
 }
 
 template<class T>
 inline TQueue<T>::~TQueue()
 {
-
+	if (arr != 0) delete[] arr;
+	start = 0;
+	finish = 0;
 }
 
 template<class T>
-inline void TQueue<T>::push(T& s)
+inline void TQueue<T>::push(T s)
 {
-
+	if ((start == finish) && (arr[finish] != 0)) throw logic_error("Full queue");
+	arr[finish] = s;
+	finish = (finish + 1) % size;
 }
 
 template<class T>
 inline T TQueue<T>::pop()
 {
+	if ((finish == start) && (arr[start] == 0)) throw logic_error("Queue is empty");
+	T a = arr[start];
+	arr[start] = 0;
+	start = (start + 1) % size;
+	return a;
+}
 
+template<class T>
+inline bool TQueue<T>::empty()
+{
+	if (start == finish)
+		return true;
+	else
+		return false;
 }
--- test/test_queue.cpp
+++ test/test_queue.cpp
@@ -2,7 +2,52 @@
 
 #include "../gtest/gtest.h"
 
-TEST(Queue, can_create_steck_with_positive_length)
+TEST(TQueue, can_create_steck_with_positive_length)
 {
   ASSERT_NO_THROW(TQueue<int> bf(3));
+}
+
+TEST(TQueue, empty_true)
+{
+  TQueue<int> A(2);
+  ASSERT_EQ(A.empty(), true);
+}
+
+TEST(TQueue, empty_false)
+{
+  TQueue<int> A(2);
+  A.push(2);
+  ASSERT_EQ(A.empty(), false);
+}
+
+TEST(TQueue, pop_take_right_element)
+{
+  TQueue<int> A(2);
+  A.push(31);
+  A.push(2);
+  ASSERT_EQ(A.pop(), 31);
+  ASSERT_EQ(A.pop(), 2);
+}
+
+TEST(TQueue, push_without_problems)
+{
+  TQueue<int> A(2);
+  ASSERT_NO_THROW(A.push(13124125));
+}
+
+TEST(TQueue, create_queue_with_uncorrect_size)
+{
+  ASSERT_ANY_THROW(TQueue<int> A(-1));
+}
+
+TEST(TQueue, push_in_out_of_size)
+{
+  TQueue<int> A(0);
+  ASSERT_ANY_THROW(A.push(13124125));
+}
+
+TEST(TQUeue, pop_in_empty_queue)
+{
+  TQueue<int> A(2);
+  ASSERT_ANY_THROW(A.pop());
 }
