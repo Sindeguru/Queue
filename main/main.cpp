94d5518414576c915b184019201f133343143030
Sindeguru <mrSinguru@gmail.com>
2020-12-17 22:56:56 +0300

    Queue on list

--- main/main.cpp
+++ main/main.cpp
@@ -6,7 +6,12 @@ using namespace std;
 
 int main()
 {
-	TQueue<int> A(5);
+
+  TQueue<int> A(2);
+  A.Push(31);
+  A.Push(2);
+  cout << A.Pop() <<' ' << 31<<endl;
+  cout << A.Pop() << ' ' <<2;
 
 	return 0;
 }
--- test/test_queue.cpp
+++ test/test_queue.cpp
@@ -1,53 +1,53 @@
 #include "queue.h"
-
 #include "../gtest/gtest.h"
+#include "queuelist.h"
 
-TEST(TQueue, can_create_steck_with_positive_length)
+TEST(TQueueList, can_create_steck_with_positive_length)
 {
-  ASSERT_NO_THROW(TQueue<int> bf(3));
+  ASSERT_NO_THROW(TQueueList<int> bf(3));
 }
 
-TEST(TQueue, empty_true)
+TEST(TQueueList, empty_true)
 {
-  TQueue<int> A(2);
+  TQueueList<int> A();
   ASSERT_EQ(A.IsEmpty(), true);
 }
 
-TEST(TQueue, empty_false)
+TEST(TQueueList, empty_false)
 {
-  TQueue<int> A(2);
+  TQueueList<int> A();
   A.Push(2);
   ASSERT_EQ(A.IsEmpty(), false);
 }
 
-TEST(TQueue, pop_take_right_element)
+TEST(TQueueList, pop_take_right_element)
 {
-  TQueue<int> A(2);
+  TQueueList<int> A(2);
   A.Push(31);
   A.Push(2);
   ASSERT_EQ(A.Pop(), 31);
   ASSERT_EQ(A.Pop(), 2);
 }
 
-TEST(TQueue, push_without_problems)
+TEST(TQueueList, push_without_problems)
 {
-  TQueue<int> A(2);
+  TQueueList<int> A(2);
   ASSERT_NO_THROW(A.Push(13124125));
 }
 
-TEST(TQueue, create_queue_with_uncorrect_size)
+TEST(TQueueList, create_queue_with_uncorrect_size)
 {
-  ASSERT_ANY_THROW(TQueue<int> A(-1));
+  ASSERT_ANY_THROW(TQueueList<int> A(-1));
 }
 
-TEST(TQueue, push_in_out_of_size)
+TEST(TQueueList, push_in_out_of_size)
 {
-  TQueue<int> A(0);
+  TQueueList<int> A(0);
   ASSERT_ANY_THROW(A.Push(13124125));
 }
 
 TEST(TQUeue, pop_in_empty_queue)
 {
-  TQueue<int> A(2);
+  TQueueList<int> A(2);
   ASSERT_ANY_THROW(A.Pop());
 }
